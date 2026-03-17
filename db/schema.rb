22d2c463d6993b1739a4565f0da3d4c04c663bbd
Grzegorz Malinowski <git.15m27@passmail.net>
2026-03-17 12:34:35 +0100

    chore: migrate to uuid

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,9 @@
 ActiveRecord::Schema[8.1].define(version: 2026_03_15_202752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pgcrypto"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "confirmation_sent_at"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
