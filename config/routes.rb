747958384eeeb6630bac9fbcb1c256dcba7f970d
Grzegorz Malinowski <git.15m27@passmail.net>
2026-03-18 08:06:20 +0100

    feat: user can create list with item from hompage

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
   get "pages/home"
 
-  resources :list_items, only: [:create]
-  resource :shopping_lists, only: [:show, :edit, :update]
+  resource :shopping_lists, only: [ :show, :edit, :update ]
+  resources :list_items, only: [ :create ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
