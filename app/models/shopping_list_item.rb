4a933bf1bf624caf7efe444dba5a5dbee2a474b6
Grzegorz Malinowski <git.15m27@passmail.net>
2026-03-19 22:13:10 +0100

    style: add styling to shopping_list

--- app/models/shopping_list_item.rb
+++ app/models/shopping_list_item.rb
@@ -2,6 +2,6 @@ class ShoppingListItem < ApplicationRecord
   belongs_to :shopping_list
   validates :name, presence: true
 
-  after_create_commit { broadcast_append_to shopping_list }
+  after_create_commit { broadcast_prepend_to shopping_list }
   after_destroy_commit { broadcast_remove_to shopping_list }
 end
