675a0c0842e84d10f74d3d3b6dff1bdfc36dc9d0
Grzegorz Malinowski <git.15m27@passmail.net>
2026-03-26 21:14:04 +0100

    feat: add group assignment form for shopping lists

--- app/controllers/group_shopping_lists_controller.rb
+++ app/controllers/group_shopping_lists_controller.rb
@@ -1,7 +1,39 @@
 
 class GroupShoppingListsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_shopping_list
 
   def edit
+    authorize @shopping_list
+    @group_shopping_lists = @shopping_list.group_shopping_lists
+    @groups = policy_scope(Group)
+  end
+
+  def update
+    authorize @shopping_list
+    allowed_params = permitted_attributes(@shopping_list)
+    allowed_params[:group_ids] = filter_group_ids(allowed_params[:group_ids])
+    if @shopping_list.update(allowed_params)
+      flash[:info] = "Shopping list updated"
+      redirect_to @shopping_list
+    else
+      @groups = policy_scope(Group)
+      flash[:alert] = "Shopping list could not be updated :("
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def filter_group_ids(group_ids)
+    ids = Array(group_ids).compact_blank
+    policy_scope(Group).where(id: ids).pluck(:id)
+  end
+
+  def set_shopping_list
+    @shopping_list = ShoppingList.find(params[:shopping_list_id])
+  end
+  def shopping_list_params
+    params.require(:shopping_list).permit(group_ids: [])
   end
 end
