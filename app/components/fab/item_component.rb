25bb88629eb8e74e32670386a63311451c1f249e
Grzegorz Malinowski <git.15m27@passmail.net>
2026-03-27 17:06:17 +0100

    feat: add customizable btn background color to FAB component

--- app/components/fab/item_component.rb
+++ app/components/fab/item_component.rb
@@ -1,7 +1,8 @@
 
 module Fab
   class ItemComponent < ViewComponent::Base
-    def initialize(icon:, path:, label:, method: :get)
+    def initialize(icon:, path:, label:, btn_type: :success, method: :get)
+      @btn_type = btn_type
       @icon = icon
       @path = path
       @label = label
