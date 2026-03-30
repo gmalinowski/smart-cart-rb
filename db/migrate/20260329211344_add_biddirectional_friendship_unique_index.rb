3d6b31f2c18ffa26d4cdec8a22b72fee53be8e72
Grzegorz Malinowski <git.15m27@passmail.net>
2026-03-30 08:33:08 +0200

    style: fix linting

--- db/migrate/20260329211344_add_biddirectional_friendship_unique_index.rb
+++ db/migrate/20260329211344_add_biddirectional_friendship_unique_index.rb
@@ -2,6 +2,5 @@ class AddBiddirectionalFriendshipUniqueIndex < ActiveRecord::Migration[8.1]
   def change
     add_index :friendships, 'LEAST(user_id::text, friend_id::text), GREATEST(user_id::text, friend_id::text)',
               unique: true, name: "index_unique_symmetric_friendships"
-
   end
 end
