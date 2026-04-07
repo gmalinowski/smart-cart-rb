0a5db0d628fd744fe109cf84be209fb853c3e1bf
Grzegorz Malinowski <git.15m27@passmail.net>
2026-04-07 22:02:18 +0200

    feat: add invitation mailer to send invitation to sign up in the app

---
+++ spec/mailers/invitation_mailer_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe InvitationMailer, type: :mailer do
+  describe "email_invitation_link" do
+    let(:inviter) { create(:user) }
+    let(:inviter_email_safe) { CGI.escapeHTML(inviter.email) }
+    let(:invitee_email) { "test@example.com" }
+    let(:invitee_email_safe) { CGI.escapeHTML(invitee_email) }
+    let(:mail) { InvitationMailer.friend_invitation_to_signup(inviter: inviter, invitee_email: invitee_email) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq(I18n.t("invitation_mailer.friend_invitation_to_signup.subject", sender: inviter.email))
+      expect(mail.to).to eq([invitee_email])
+    end
+
+    it "renders the body" do
+      expected_text = I18n.t("invitation_mailer.friend_invitation_to_signup.body", sender: inviter_email_safe)
+      expect(mail.body.encoded).to include(expected_text)
+    end
+
+    it "includes sender's email in the body" do
+      expect(mail.body.encoded).to include(inviter_email_safe)
+    end
+
+    it 'includes link to sign up page' do
+      expect(mail.body.encoded).to include(new_user_registration_url)
+    end
+  end
+
+end
---
+++ spec/mailers/previews/invitation_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer_mailer
+class InvitationMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/invitation_mailer_mailer/email_invitation_link
+  def friend_invitation_to_signup
+    InvitationMailer.friend_invitation_to_signup(inviter: User.first, invitee_email: "test@test.com")
+  end
+
+end
